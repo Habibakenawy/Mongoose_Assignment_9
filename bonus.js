80ea7a40d5062f731d45f3ca0b755c7d44ecb476
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2026-07-10 20:19:12 +0300

    Create bonus.js

---
+++ bonus.js
@@ -0,0 +1,11 @@
+    let prefix=strs[0];
+
+    for(let i=1;i<strs.length;i++){
+        while(!(strs[i].startsWith(prefix))){
+            prefix=prefix.substring(0,prefix.length-1);
+            if(prefix === ""){
+                return "";
+            }
+        }
+    }
+    return prefix;
